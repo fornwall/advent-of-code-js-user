aec4f988ed99bd2bea0302a87acaa2c36b2b6790
Fredrik Fornwall <fredrik@fornwall.net>
2022-11-05 11:41:21 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+generate:
+	./generate-tests-from-aoc.py > src/generated.test.js
+
+.PHONY: generate
+
---
+++ src/sum.test.js
@@ -0,0 +1,9 @@
+const solve = require("advent-of-code-wasm").solve;
+
+//const solve = require("advent-of-code-wasm").solve;
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(solve(2019, 1, 1, "14")).toBe("2");
+
+  expect(1 + 2).toBe(3);
+});
