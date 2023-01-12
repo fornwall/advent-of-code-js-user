8bc4fab7987da000bb10e6a4070cc06f513d4997
Fredrik Fornwall <fredrik@fornwall.net>
2023-01-12 23:51:28 +0100

    Improve test

---
+++ src/manual.test.js
@@ -0,0 +1,14 @@
+const solve = require("advent-of-code-wasm").solve;
+test('Basic functionality works', () => {
+  expect(solve(2019, 1, 1, "14")).toBe("2");
+});
+
+test('Exception is thrown on bad input', () => {
+  const s = () => { solve(2019, 1, 1, "bad_input"); };
+  expect(s).toThrow("Line 1: Invalid digit found in string");
+});
+
+test('Exception is thrown on invalid year', () => {
+  const s = () => { solve(2010, 1, 1, "14"); };
+  expect(s).toThrow("Unsupported year=2010, day=1, part=1");
+});
